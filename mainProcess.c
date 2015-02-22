de831ba2593c62792a257a73dda99c0046d8d953
Guy Avimor <guy.avimor@gmail.com>
2015-02-22 09:13:24 +0200

    Update mainProcess.c

--- mainProcess.c
+++ mainProcess.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
         {
             if (currAssemblyFile = fopen(argv[i],READ))
             {
-                process(currAssemblyFile,fileName);//  TODO: write the process for assembly files
+                processFile(currAssemblyFile,fileName);//  TODO: write the process for assembly files
                 fclose(currAssemblyFile);
             }
             free(fileName);
