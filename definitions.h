169946a2162debb373688c2b1cbf5d8d00e8907a
GuyAvimor <guy.avimor@gmail.com>
2015-02-22 20:29:06 +0200

    start here

--- definitions.h
+++ definitions.h
@@ -11,11 +11,16 @@
 #include <string.h>
 
 #define ASSEMBLY ".as"
+#define OBJECT ".ob"
+#define EXTERNALS ".ext"
+#define ENTRIES ".ent"
 #define READ "r" /* for fopen */
 #define WRITE "w" /* for fopen */
 #define A 0 /* absolute */
 #define E 1 /* external */
 #define R 2 /* relocatable */
+enum {FIRST,SECOND,THIRD} /* group number */
+enum {IMMEDIATE,DIRECT,DISTANCE,REGISTER} /* Addressing methods */
 
 #ifndef opcodes
 char *opcodes[16] =
@@ -38,11 +43,11 @@ char *opcodes[16] =
 #endif
 
 typedef struct {
-    int group :2; /* the amount of operands for this instruction */
-    int opcode:4;
-    int source:2; /* source operand */
-    int target:2; /* target operand */
-    int mode  :2; /* E/R/A */
+    int mode   :2; /* E/R/A */
+    int target :2; /* target operand Addressing method*/
+    int source :2; /* source operand Addressing method*/
+    int opcode :4;
+    int group  :2; /* the amount of operands for this instruction */
 } instr_h; /* the instruction header - the first word of each instruction */
 
 void process (FILE *file, char *fileName);
---
+++ fileProcess.h
@@ -0,0 +1,22 @@
+//
+//  fileProcess.h
+//  
+//
+//  Created by Guy on 2/22/15.
+//
+//
+
+#include "definitions.h"
+
+typedef struct Node
+{
+    char *name;
+    unsigned long int address;
+    struct Node *next;
+}Simbol;
+
+typedef struct
+{
+    Simbol *head;
+}SimbolList;
+
