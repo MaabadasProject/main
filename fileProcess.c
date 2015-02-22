29b152af1e44c66fa6bef1241073b4f1971969e7
GuyAvimor <guy.avimor@gmail.com>
2015-02-23 00:56:43 +0200

    start of process

--- fileProcess.c
+++ fileProcess.c
@@ -1,10 +1,3 @@
-//
-//  fileProcess.c
-//  
-//
-//  Created by Guy on 2/22/15.
-//
-//
 
 #include "fileProcess.h"
 
@@ -12,7 +5,13 @@
 /* this will get an assembly file (the first argument) ready for reading, and the files name. */
 void processFile (FILE *asFile, char *fileName)
 {
+    SymbolList symbols;
     
+    symbols = symbol_list(asFile);
+    
+    /* TOBE: continue... */
+    
+    free_list(symbols);
 }
 
 /* this will parse an opcode and return it's position in the table opcodes. */
@@ -39,4 +38,73 @@ int get_opcode()
   }
   puts("unrecognized opcode");
   return -1;
+}
+
+/* this will return a SymbolList contains all the symbols in the assembly file */
+SymbolList symbol_list (FILE *asFile)
+{
+    SymbolList list;
+    Symbol *currSymbol, *lastSymbol;
+    
+    &list = new_symbolList();
+    fseek(asFile,0L,SEEK_SET);
+    
+    list.head = get_symbol_name(asFile,ftell(asFile),list); /* TODO: write this function */
+    lastSymbol = list.head;
+    if (!lastSymbol)
+    {
+        while ((currSymbol = get_symbol_name(asFile,ftell(asFile),list))) /* TODO: write this function */
+        {
+            lastSymbol->next = currSymbol;
+            lastSymbol = lastSymbol->next;
+        }
+    }
+    return list;
+}
+
+/* returns a pointer to a new SymbolList created using malloc */
+SymbolList * new_symbolList()
+{
+    SymbolList *list;
+    list = (SymbolList *)malloc(sizeof(SymbolList));
+    list->head = NULL;
+    return list;
+}
+
+/* returns a pointer to a new Symbol created using malloc */
+/* symName should by shorter than 31 chars */
+Symbol * new_symbol(char *symName, unsigned long int symAddress)
+{
+    Symbol *sym;
+    sym = (Symbol *)malloc(sizeof(Symbol));
+    strcpy(sym->name,symName);
+    sym->address = symAddress;
+    sym->next = NULL;
+    return sym;
+}
+
+/* this will free the symbols list */
+void free_list (SymbolList list)
+{
+    if (list.head)
+    {
+        Symbol *curr, *post;
+        curr = list.head;
+        post = curr->next;
+        while (post)
+        {
+            free(curr);
+            curr = post;
+            post = post->next;
+        }
+        free(curr);
+    }
+    free(list);
+}
+
+/* this will return each time a new symbol */
+/* returns NULL if there are no more symbols in the file */
+Symbol * get_symbol_name (FILE *asFile, long startPos, SymbolList knoneSymbols)
+{
+    /*TODO: write this function */
 }
