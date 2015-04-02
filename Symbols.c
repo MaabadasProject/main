39c36fa55c6f2f6f7af281c8523820dee1802e9a
GuyAvimor <guy.avimor@gmail.com>
2015-04-02 20:03:21 +0300

    finally
    almost can build the symbol table. also filesBuilder.c and .h was added
    and few functions was moved to there.

--- Symbols.c
+++ Symbols.c
@@ -1,11 +1,41 @@
 
 #include "Symbols.h"
 
-SymbolList symbols_list(My_File file)
+SymbolList * symbols_list(My_File *file)
 {
-    SymbolList list;
+    SymbolList *list;
+    Symbol *currSym, *dummy;
+    My_Line *currLine;
+    int wordCount;
     
-    &list = new_symbolList();
+    list = new_symbolList();
+    dummy->next = NULL;
+    currSym = dummy;
+    wordCount = FIRST_LINE;
+    
+    for (currLine = file->head; currLine; currLine++)
+    {
+        if (currLine->label)
+        {
+            currSym->next = new_symbol(currLine->label,wordCount);
+            currSym = currSym->next;
+        }
+        else if (currLine->kind == Request && currLine->statement.request.kind == EXTERN)
+        {
+            currSym->next = new_symbol(currLine->statement.request.data.str,0);
+            currSym = currSym->next;
+        }
+        wordCount += increaseBy(currLine);
+    }
+    
+    list->head = dummy->next;
+    return list;
+}
+
+/* returns the number of lines the line will be in the object file */
+int increaseBy (My_Line *line)
+{
+    //TODO: write the func
 }
 
 /* this will free the symbols list */
@@ -38,12 +68,12 @@ SymbolList * new_symbolList()
 
 /* returns a pointer to a new Symbol created using malloc */
 /* symName should by shorter than 31 chars */
-Symbol * new_symbol(char *symName, long symValue)
+Symbol * new_symbol(char *symName, int symValue)
 {
     /* TODO: write when it's time */
 	Symbol *p;
 	p = (Symbol *) malloc (sizeof(Symbol *));
-	p->name = symName; // maybe copy it instead of rereferencing it?
+    p->name = symName;
 	p->value = symValue;
 	p->next = NULL;
 	return p;
